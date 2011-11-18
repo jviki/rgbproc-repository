f2bebe18224cff380fc5976b1a30004c112492e3
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-11-18 10:44:53 +0100

    Changed semantics of rgb_asfifo

--- hdl/vhdl/rgb_asfifo.vhd
+++ hdl/vhdl/rgb_asfifo.vhd
@@ -11,20 +11,20 @@ port (
 	ASYNC_RST     : in  std_logic;
 
 	RGB_IN_CLK    : in  std_logic;
-	RGB_IN_RED    : in  std_logic_vector(7 downto 0);
-	RGB_IN_GREEN  : in  std_logic_vector(7 downto 0);
-	RGB_IN_BLUE   : in  std_logic_vector(7 downto 0);
-	RGB_IN_HSYNC  : in  std_logic;
-	RGB_IN_VSYNC  : in  std_logic;
+	RGB_IN_R      : in  std_logic_vector(7 downto 0);
+	RGB_IN_G      : in  std_logic_vector(7 downto 0);
+	RGB_IN_B      : in  std_logic_vector(7 downto 0);
+	RGB_IN_EOL    : in  std_logic;
+	RGB_IN_EOF    : in  std_logic;
 	RGB_IN_WE     : in  std_logic;
 	RGB_IN_FULL   : out std_logic;
 
 	RGB_OUT_CLK   : in  std_logic;
-	RGB_OUT_RED   : out std_logic_vector(7 downto 0);
-	RGB_OUT_GREEN : out std_logic_vector(7 downto 0);
-	RGB_OUT_BLUE  : out std_logic_vector(7 downto 0);
-	RGB_OUT_HSYNC : out std_logic;
-	RGB_OUT_VSYNC : out std_logic;
+	RGB_OUT_R     : out std_logic_vector(7 downto 0);
+	RGB_OUT_G     : out std_logic_vector(7 downto 0);
+	RGB_OUT_B     : out std_logic_vector(7 downto 0);
+	RGB_OUT_EOL   : out std_logic;
+	RGB_OUT_EOF   : out std_logic;
 	RGB_OUT_RE    : in  std_logic;
 	RGB_OUT_EMPTY : out std_logic
 );
@@ -66,17 +66,17 @@ begin
 		EMPTY  => RGB_OUT_EMPTY
 	);
 
-	rgb_din(25 downto 18) <= RGB_IN_RED;
-	rgb_din(17 downto 10) <= RGB_IN_GREEN;
-	rgb_din( 9 downto  2) <= RGB_IN_BLUE;
-	rgb_din(1) <= RGB_IN_HSYNC;
-	rgb_din(0) <= RGB_IN_VSYNC;
+	rgb_din(25 downto 18) <= RGB_IN_R;
+	rgb_din(17 downto 10) <= RGB_IN_G;
+	rgb_din( 9 downto  2) <= RGB_IN_B;
+	rgb_din(1) <= RGB_IN_EOL;
+	rgb_din(0) <= RGB_IN_EOF;
 
-	RGB_OUT_RED   <= rgb_dout(25 downto 18);
-	RGB_OUT_GREEN <= rgb_dout(17 downto 10);
-	RGB_OUT_BLUE  <= rgb_dout( 9 downto  2);
-	RGB_OUT_HSYNC <= rgb_dout(1);
-	RGB_OUT_VSYNC <= rgb_dout(0);
+	RGB_OUT_R   <= rgb_dout(25 downto 18);
+	RGB_OUT_G   <= rgb_dout(17 downto 10);
+	RGB_OUT_B   <= rgb_dout( 9 downto  2);
+	RGB_OUT_EOL <= rgb_dout(1);
+	RGB_OUT_EOF <= rgb_dout(0);
 
 end architecture;
 
