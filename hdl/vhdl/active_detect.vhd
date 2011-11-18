15b790188fa57cd79f6767e19eeb212c852321e0
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-11-18 12:11:59 +0100

    Fixed include of proc_commons_v3_00_a library

--- hdl/vhdl/active_detect.vhd
+++ hdl/vhdl/active_detect.vhd
@@ -6,8 +6,8 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
-library proc_commons_v3_00_a;
-use proc_commons_v3_00_a.proc_commons_pkg.log2;
+library proc_common_v3_00_a;
+use proc_common_v3_00_a.proc_common_pkg.log2;
 
 ---
 -- Detects active phase in which the data (pixels) are passed. 
