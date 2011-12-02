bee0d2931acd3994e8557b70e6c4adceab614edf
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-12-02 17:10:40 +0100

    XPS wrapper for low pass filter (stub)

---
+++ rgbproc/pcores/xps_lowpass_filter_v1_00_a/hdl/vhdl/xps_lowpass_filter.vhd
@@ -0,0 +1,10 @@
+-- xps_lowpass_filter.vhd
+-- Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+
+
