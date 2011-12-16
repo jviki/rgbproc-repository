2f8a430750ed357382cd9e084106275ed92a929d
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-12-16 17:26:35 +0100

    Stub of rgb_split

---
+++ rgbproc/pcores/rgb_split_v1_00_a/hdl/vhdl/rgb_split.vhd
@@ -0,0 +1,14 @@
+-- rgb_split.vhd
+-- Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity rgb_split is
+port (
+	CLK : in  std_logic	
+);
+end entity;
+
