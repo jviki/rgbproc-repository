192d5cffe95c4ea698c6328289d1a4aaeaf25977
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-12-17 16:39:43 +0100

    Fix of transition of the multiplexor
    - transition occures only during H+V pulse on the
      current active source
    - the transition finishes when the new source's H+V
      pulse starts
    - there is a period when none of the sources is used
      it may result in a longer H+V pulse then it is usual
    - when transiting the pulse longs always at least the
      same time as the pulse of the new source

--- rgbproc/pcores/rgb_mux_v1_00_a/hdl/vhdl/rgb_mux.vhd
+++ rgbproc/pcores/rgb_mux_v1_00_a/hdl/vhdl/rgb_mux.vhd
@@ -42,11 +42,40 @@ end entity;
 
 architecture full of rgb_mux is
 
-	type state_t is (s_src0, s_src1, s_wait_eof0, s_wait0, s_wait_eof1, s_wait1);
+	type state_t is (s_src0, s_src1, s_sync0_to_1, s_0_to_sync1, s_sync1_to_0, s_1_to_sync0);
 	signal state  : state_t;
 	signal nstate : state_t;
 
+	signal sync0        : std_logic;
+	signal sync1        : std_logic;
+	signal pulse_start0 : std_logic;
+	signal pulse_start1 : std_logic;
+
+
 begin
+
+	sync0 <= IN0_HS or IN0_VS;
+	sync1 <= IN1_HS or IN1_VS;
+
+	----------------------------------
+
+	detect_start_of_pulse0 : entity work.detect_start_of_pulse
+	port map (
+		CLK  => CLK,
+		RST  => RST,
+		SYNC => sync0,
+		SOP  => pulse_start0
+	);
+
+	detect_start_of_pulse1 : entity work.detect_start_of_pulse
+	port map (
+		CLK  => CLK,
+		RST  => RST,
+		SYNC => sync1,
+		SOP  => pulse_start1
+	);
+
+	----------------------------------
 
 	fsm_state : process(CLK, RST, nstate)
 	begin
@@ -70,39 +99,48 @@ begin
 		case state is
 		when s_src0 =>
 			if SRC_SEL = '1' then
-				nstate <= s_wait_eof1;
+				nstate <= s_sync0_to_1;
 			end if;
 
-		when s_wait_eof1 =>
+		when s_sync0_to_1 =>
 			if IN0_HS = '0' and IN0_VS = '0' then
-				nstate <= s_wait1;
+				nstate <= s_0_to_sync1;
 			end if;
 
-		when s_wait1 =>
-			if IN0_HS = '1' and IN0_VS = '1' then
+		when s_0_to_sync1 =>
+			if pulse_start1 = '1' then
 				nstate <= s_src1;
 			end if;
 
 		when s_src1 =>
 			if SRC_SEL = '0' then
-				nstate <= s_wait0;
+				nstate <= s_sync1_to_0;;
 			end if;
 
-		when s_wait_eof0 =>
+		when s_sync1_to_0 =>
 			if IN1_HS = '0' and IN1_VS = '0' then
-				nstate <= s_wait0;
+				nstate <= s_1_to_sync0;
 			end if;
 
-		when s_wait0 =>
-			if IN1_HS = '1' and IN1_VS = '1' then
+		when s_1_to_sync0 =>
+			if pulse_start0 = '1' then
 				nstate <= s_src0;
+			end if;
+
 		end case;
 	end process;
 
 	fsm_output : process(CLK, state)
 	begin
+		OUT_R <= (others => 'X');
+		OUT_G <= (others => 'X');
+		OUT_B <= (others => 'X');
+		OUT_DE <= '0';
+		OUT_HS <= '0';
+		OUT_VS <= '0';
+
 		case state is
-		when s_src0 | s_wait_eof1 | s_wait1 =>
+		when s_src0 | s_sync0_to_1 =>
 			OUT_R  <= IN0_R;
 			OUT_G  <= IN0_G;
 			OUT_B  <= IN0_B;
@@ -111,13 +149,19 @@ begin
 			OUT_VS <= IN0_VS;
 			CUR_SEL <= '0';
 
-		when others => -- s_src1 | s_wait_eof0 | s_wait0
+		when s_0_to_sync1 =>
+			CUR_SEL <= '0';
+
+		when s_src1 | s_sync1_to_0 =>
 			OUT_R  <= IN1_R;
 			OUT_G  <= IN1_G;
 			OUT_B  <= IN1_B;
 			OUT_DE <= IN1_DE;
 			OUT_HS <= IN1_HS;
 			OUT_VS <= IN1_VS;
+			CUR_SEL <= '1';
+
+		when s_1_to_sync0 =>
 			CUR_SEL <= '1';
 			
 		end case;
