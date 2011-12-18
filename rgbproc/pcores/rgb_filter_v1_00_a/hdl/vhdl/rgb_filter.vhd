639eae3766f6366ea4ec974733ec16c08328c419
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-12-18 12:07:23 +0100

    Implementation of the filter

--- rgbproc/pcores/rgb_filter_v1_00_a/hdl/vhdl/rgb_filter.vhd
+++ rgbproc/pcores/rgb_filter_v1_00_a/hdl/vhdl/rgb_filter.vhd
@@ -37,7 +37,47 @@ port (
 end entity;
 
 architecture full of rgb_filter is
+
+	signal filtered_r : std_logic_vector(7 downto 0);
+	signal filtered_g : std_logic_vector(7 downto 0);
+	signal filtered_b : std_logic_vector(7 downto 0);
+
 begin
+
+	output_regp : process(CLK, CE, filtered_r, filtered_g, filtered_b)
+	begin
+		if rising_edge(CLK) then
+			if CE = '1' then
+				OUT_R  <= filtered_r;
+				OUT_G  <= filtered_g;
+				OUT_B  <= filtered_b;
+				OUT_DE <= IN_DE;
+				OUT_HS <= IN_HS;
+				OUT_VS <= IN_VS;
+			end if;
+		end if;
+	end process;
+
+gen_and: if OPERATION = OP_AND
+generate
+	filtered_r <= IN_R and RED;
+	filtered_g <= IN_G and GREEN;
+	filtered_b <= IN_B and BLUE;
+end generate;
+
+gen_or: if OPERATION = OP_OR
+generate
+	filtered_r <= IN_R or RED;
+	filtered_g <= IN_G or GREEN;
+	filtered_b <= IN_B or BLUE;
+end generate;
+
+gen_xor: if OPERATION = OP_XOR
+generate
+	filtered_r <= IN_R xor RED;
+	filtered_g <= IN_G xor GREEN;
+	filtered_b <= IN_B xor BLUE;
+end generate;
 
 end architecture;
 
