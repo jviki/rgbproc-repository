e3ff8025dc76684a5132426205a3a33bf42ce03f
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2012-01-05 00:22:38 +0100

    Asynchronous FIFO with NGC implementations

--- rgbproc/pcores/utils_v1_00_a/hdl/vhdl/afifo.vhd
+++ rgbproc/pcores/utils_v1_00_a/hdl/vhdl/afifo.vhd
@@ -27,6 +27,80 @@ port (
 end entity;
 
 architecture full of afifo is
+	COMPONENT afifo_35b_16
+	  PORT (
+	    rst : IN STD_LOGIC;
+	    wr_clk : IN STD_LOGIC;
+	    rd_clk : IN STD_LOGIC;
+	    din : IN STD_LOGIC_VECTOR(34 DOWNTO 0);
+	    wr_en : IN STD_LOGIC;
+	    rd_en : IN STD_LOGIC;
+	    dout : OUT STD_LOGIC_VECTOR(34 DOWNTO 0);
+	    full : OUT STD_LOGIC;
+	    empty : OUT STD_LOGIC
+	  );
+	END COMPONENT;
+
+	COMPONENT afifo_70b_16
+	  PORT (
+	    rst : IN STD_LOGIC;
+	    wr_clk : IN STD_LOGIC;
+	    rd_clk : IN STD_LOGIC;
+	    din : IN STD_LOGIC_VECTOR(69 DOWNTO 0);
+	    wr_en : IN STD_LOGIC;
+	    rd_en : IN STD_LOGIC;
+	    dout : OUT STD_LOGIC_VECTOR(69 DOWNTO 0);
+	    full : OUT STD_LOGIC;
+	    empty : OUT STD_LOGIC
+	  );
+	END COMPONENT;
 begin
+
+	assert (DWIDTH = 35 and DEPTH = 16)
+	    or (DWIDTH = 70 and DEPTH = 16)
+		report "Invalid generic configuration: "
+		     & integer'image(DWIDTH) & ", "
+		     & integer'image(DEPTH)
+		severity failure;
+
+	--------------------------------------
+
+gen_35b_16_afifo: if DWIDTH = 35 and DEPTH = 16
+generate
+
+	impl_i : afifo_35b_16
+	port map (
+		rst    => RESET,
+		wr_clk => WCLK,
+		rd_clk => RCLK,
+		din    => DI,
+		wr_en  => WE,
+		full   => FULL,
+		dout   => DO,
+		rd_en  => RE,
+		empty  => EMPTY
+	);
+
+end generate;
+
+	--------------------------------------
+
+gen_70b_16_afifo: if DWIDTH = 70 and DEPTH = 16
+generate
+
+	impl_i : afifo_70b_16
+	port map (
+		rst    => RESET,
+		wr_clk => WCLK,
+		rd_clk => RCLK,
+		din    => DI,
+		wr_en  => WE,
+		full   => FULL,
+		dout   => DO,
+		rd_en  => RE,
+		empty  => EMPTY
+	);
+
+end generate;
 
 end architecture;
