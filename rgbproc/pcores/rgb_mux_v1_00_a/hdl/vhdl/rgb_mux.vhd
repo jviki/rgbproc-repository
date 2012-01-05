595822b560d8d25382dd0ceee95ea5332ec74a7c
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2012-01-05 12:51:14 +0100

    RGB MUX is controlled by IPIF, not finished yet

--- rgbproc/pcores/rgb_mux_v1_00_a/hdl/vhdl/rgb_mux.vhd
+++ rgbproc/pcores/rgb_mux_v1_00_a/hdl/vhdl/rgb_mux.vhd
@@ -6,16 +6,28 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
+library utils_v1_00_a;
+use utils_v1_00_a.ipif_reg;
+
 entity rgb_mux is
 generic (
+	IPIF_AWIDTH : integer := 32;
+	IPIF_DWIDTH : integer := 32;
 	DEFAULT_SRC : integer := 0
 );
 port (
 	CLK     : in  std_logic;
 	RST     : in  std_logic;
 
-	SRC_SEL : in  std_logic;
-	CUR_SEL : out std_logic;
+	Bus2IP_Addr  : out std_logic_vector(IPIF_AWIDTH - 1 downto 0);
+	Bus2IP_CS    : out std_logic_vector(0 downto 0);
+	Bus2IP_RNW   : out std_logic;
+	Bus2IP_Data  : out std_logic_vector(IPIF_DWIDTH - 1 downto 0);
+	Bus2IP_BE    : out std_logic_vector(IPIF_DWIDTH/8 - 1 downto 0);
+	IP2Bus_Data  : in  std_logic_vector(IPIF_DWIDTH - 1 downto 0);
+	IP2Bus_RdAck : in  std_logic;
+	IP2Bus_WrAck : in  std_logic;
+	IP2Bus_Error : in  std_logic;
 
 	IN0_R   : in  std_logic_vector(7 downto 0);
 	IN0_G   : in  std_logic_vector(7 downto 0);
@@ -51,8 +63,83 @@ architecture full of rgb_mux is
 	signal pulse_start0 : std_logic;
 	signal pulse_start1 : std_logic;
 
+	signal src_sel      : std_logic;
+	signal cur_sel      : std_logic;
 
 begin
+
+	---
+	-- Device ID register
+	---
+	reg_id : utils_v1_00_a.ipif_reg
+	generic map (
+		REG_DWIDTH  => 32,
+		REG_DEFAULT => 1,
+		IPIF_DWIDTH => IPIF_DWIDTH,
+		IPIF_MODE   => 0
+	)
+	port map (
+		CLK          => CLK,
+		RST          => RST,
+
+		IP2Bus_Data  => ipif_data(31 downto 0),
+		IP2Bus_WrAck => ipif_wrack(0),
+		IP2Bus_RdAck => ipif_rdack(0),
+		IP2Bus_Error => ipif_error(0),		
+		Bus2IP_Data  => Bus2IP_Data,
+		Bus2IP_BE    => Bus2IP_BE,
+		Bus2IP_RNW   => Bus2IP_RNW,
+		Bus2IP_CS    => ipif_cs(0),
+
+		REG_DI       => (others => 'X'),
+		REG_WE       => '0'
+	);
+
+	---
+	-- MUX select register
+	---
+	cfg_sel : utils_v1_00_a.ipif_reg
+	generic map (
+		REG_DWIDTH  => 1,
+		REG_DEFAULT => DEFAULT_SRC,
+		IPIF_DWIDTH => IPIF_DWIDTH,
+		IPIF_MODE   => 2
+	)
+	port map (
+		CLK          => CLK,
+		RST          => RST,
+
+		IP2Bus_Data  => ipif_data(63 downto 32),
+		IP2Bus_WrAck => ipif_wrack(1),
+		IP2Bus_RdAck => ipif_rdack(1),
+		IP2Bus_Error => ipif_error(1),		
+		Bus2IP_Data  => Bus2IP_Data,
+		Bus2IP_BE    => Bus2IP_BE,
+		Bus2IP_RNW   => Bus2IP_RNW,
+		Bus2IP_CS    => ipif_cs(1),
+
+		REG_DI       => (others => 'X'),
+		REG_WE       => '0'
+	);
+
+	---
+	-- IPIF address logic
+	---
+
+	ipif_cs(0)  <= Bus2IP_CS(0) when Bus2IP_Addr = X"00000000" else '0';
+	ipif_cs(1)  <= Bus2IP_CS(0) when Bus2IP_Addr = X"00000004" else '0';
+	
+	-- invalid address request
+	ipif_gerror <= Bus2IP_CS(0) and (ipif_cs = "00");
+
+	IP2Bus_Data <= ipif_data(63 downto 32) when ipif_cs = "10" else
+                    <= ipif_data(31 downto  0);
+
+	IP2Bus_WrAck <= ipif_wrack(0) or ipif_wrack(1);
+	IP2Bus_RdAck <= ipif_rdack(0) or ipif_rdack(1);
+	IP2Bus_Error <= ipif_error(0) or ipif_error(1) or ipif_gerror;
+
+	----------------------------------
 
 	sync0 <= IN0_HS or IN0_VS;
 	sync1 <= IN1_HS or IN1_VS;
@@ -92,17 +179,17 @@ begin
 		end if;
 	end process;
 
-	fsm_next : process(CLK, state, SRC_SEL, IN0_HS, IN0_VS, IN1_HS, IN1_VS)
+	fsm_next : process(CLK, state, src_sel, IN0_HS, IN0_VS, IN1_HS, IN1_VS)
 	begin
 		nstate <= state;
 
 		case state is
 		when s_src0 =>
-			if SRC_SEL = '1' and IN0_HS = '0' and IN0_VS = '0' and pulse_start1 = '1' then
+			if src_sel = '1' and IN0_HS = '0' and IN0_VS = '0' and pulse_start1 = '1' then
 				nstate <= s_src1;
-			elsif SRC_SEL = '1' and IN0_HS = '0' and IN0_VS = '0' then
+			elsif src_sel = '1' and IN0_HS = '0' and IN0_VS = '0' then
 				nstate <= s_0_to_sync1;
-			elsif SRC_SEL = '1' then
+			elsif src_sel = '1' then
 				nstate <= s_sync0_to_1;
 			end if;
 
@@ -117,11 +204,11 @@ begin
 			end if;
 
 		when s_src1 =>
-			if SRC_SEL = '0' and IN1_HS = '0' and IN1_VS = '0' and pulse_start0 = '1' then
+			if src_sel = '0' and IN1_HS = '0' and IN1_VS = '0' and pulse_start0 = '1' then
 				nstate <= s_src0;
-			elsif SRC_SEL = '0' and IN1_HS = '0' and IN1_VS = '0' then
+			elsif src_sel = '0' and IN1_HS = '0' and IN1_VS = '0' then
 				nstate <= s_1_to_sync0;
-			elsif SRC_SEL = '0' then
+			elsif src_sel = '0' then
 				nstate <= s_sync1_to_0;;
 			end if;
 
@@ -155,10 +242,10 @@ begin
 			OUT_DE <= IN0_DE;
 			OUT_HS <= IN0_HS;
 			OUT_VS <= IN0_VS;
-			CUR_SEL <= '0';
+			cur_sel <= '0';
 
 		when s_0_to_sync1 =>
-			CUR_SEL <= '0';
+			cur_sel <= '0';
 
 		when s_src1 | s_sync1_to_0 =>
 			OUT_R  <= IN1_R;
@@ -167,10 +254,10 @@ begin
 			OUT_DE <= IN1_DE;
 			OUT_HS <= IN1_HS;
 			OUT_VS <= IN1_VS;
-			CUR_SEL <= '1';
+			cur_sel <= '1';
 
 		when s_1_to_sync0 =>
-			CUR_SEL <= '1';
+			cur_sel <= '1';
 			
 		end case;
 	end process;
