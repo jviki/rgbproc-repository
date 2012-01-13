8d42515a573381f16641e0b5db33207359a90241
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2012-01-13 16:54:04 +0100

    Fix of verifying testbench

--- rgbproc/pcores/rgb_gen_v1_00_a/sim/rgb_gen_tb.vhd
+++ rgbproc/pcores/rgb_gen_v1_00_a/sim/rgb_gen_tb.vhd
@@ -49,38 +49,48 @@ begin
 		RST => rst
 	);
 
-	horizp : process(CLK, RST)
+	horizp : process(CLK, RST, gen_hs, gen_vs)
+		variable init : boolean := false;
 	begin
 		if rising_edge(CLK) then
-			if RST = '1' or (gen_hs = '0' and gen_vs = '0') then
+			if RST = '1' or (gen_vs = '0' and not init) then
 				horiz <= 0;
+			elsif gen_vs = '1' and not init then
+				init := true;
+				horiz <= (horiz + 1) mod 800;
 			else
-				horiz <= horiz + 1;
-				horiz <= horiz mod 800;
+				horiz <= (horiz + 1) mod 800;
 			end if;
 		end if;
 	end process;
 
-	vertp : process(CLK, RST)
+	vertp : process(CLK, RST, horiz)
 	begin
 		if rising_edge(CLK) then
-			if RST = '1' or (gen_hs = '0' and gen_vs = '0') then
+			if RST = '1' then
 				vert <= 0;
 			elsif horiz = 799 then
-				vert <= vert + 1;
-				vert <= vert mod 525;
+				vert <= (vert + 1) mod 525;
 			end if;
 		end if;
 	end process;
 
-	x_hs <= '0' when horiz >= 48 + 640 + 16 else '1';
+	x_hs <= '0' when horiz < 96 else '1';
 	x_vs <= '0' when vert  >= 33 + 480 + 10 else '1';
-	x_de <= x_vs when horiz >= 48 and horiz < 48 + 640 else '0';
+	x_de <= '1' when horiz >= 96 + 48 and horiz < 96 + 48 + 640
+	             and vert  >= 33 and vert  < 33 + 480 else '0';
 
 	checkp : process(CLK, RST, gen_hs, x_hs, gen_vs, x_vs, gen_de, x_de)
+		variable init : boolean := false;
 	begin
 		if rising_edge(CLK) then
-			if RST = '0' then
+			if RST = '1' then
+				init := false;
+			elsif gen_hs = '1' and gen_vs = '1' then
+				init := true;
+			end if;
+
+			if RST = '0' and init then
 				assert gen_hs = x_hs
 					report "Invalid horizontal synchronization"
 					severity failure;
